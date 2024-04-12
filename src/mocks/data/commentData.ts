ee00791f01c186fad5ed33352336cd7dee91b4fb
devchaen <cogus1221@gmail.com>
2024-04-12 15:08:19 +0900

    type error 방지를 위해 mock service 관련 코드 주석처리

--- src/mocks/data/commentData.ts
+++ src/mocks/data/commentData.ts
@@ -1,24 +1,24 @@
-const contents = [
-  'hello',
-  'world',
-  'this is a comment',
-  'I like this post',
-  'I don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this post',
-  'I’m indifferent to this post',
-];
+// const contents = [
+//   'hello',
+//   'world',
+//   'this is a comment',
+//   'I like this post',
+//   'I don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this postI don’t care about this post',
+//   'I’m indifferent to this post',
+// ];
 
-const usernames = [
-  'John',
-  'Jane',
-  'Doe',
-  'Alice',
-  'Bob',
-  'Charlie',
-  'David',
-  'Eve',
-  'Frank',
-  'Grace',
-];
+// const usernames = [
+//   'John',
+//   'Jane',
+//   'Doe',
+//   'Alice',
+//   'Bob',
+//   'Charlie',
+//   'David',
+//   'Eve',
+//   'Frank',
+//   'Grace',
+// ];
 
 export type Comment = {
   id: number;
@@ -33,22 +33,22 @@ export type Comment = {
 };
 
 // Remove the existing declaration of comments: const comments: Comment[] = [];
-const comments = { results: [] as Comment[] };
-comments.results = [];
+// const comments = { results: [] as Comment[] };
+// comments.results = [];
 
-for (let i = 0; i <= 50; i++) {
-  const comment: Comment = {
-    id: i,
-    content: contents[i % contents.length],
-    likeCNT: 20,
-    createDate: '2024-03-14T21:37:21.988414',
-    userId: i,
-    userImage: 'https://placehold.co/40x40',
-    username: usernames[i % usernames.length] + i,
-    postId: i,
-    liked: i % 2 === 0 ? true : false,
-  };
-  comments.results.push(comment);
-}
+// for (let i = 0; i <= 50; i++) {
+//   const comment: Comment = {
+//     id: i,
+//     content: contents[i % contents.length],
+//     likeCNT: 20,
+//     createDate: '2024-03-14T21:37:21.988414',
+//     userId: i,
+//     userImage: 'https://placehold.co/40x40',
+//     username: usernames[i % usernames.length] + i,
+//     postId: i,
+//     liked: i % 2 === 0 ? true : false,
+//   };
+//   comments.results.push(comment);
+// }
 
-export default comments;
+// export default comments;
